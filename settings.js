1647daa8c75801bf210c50df0ebb1b6bcd5150d3
Botwangsap <107746504+Botwangsap@users.noreply.github.com>
2022-07-29 05:55:01 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,8 +13,8 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read pesan / message
-global.autobio = true 
+global.autoread = true // auto read pesan / message
+global.autobio = false 
 
 //language
 //Available in indonesia , english & spanyol 
@@ -24,29 +24,29 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Adelbot-MD~" //namabot kalian
+global.ownername= "𝓛𝓸𝓵𝓲𝓒𝓱𝓪𝓷රධ" //nama kalian
+global.myweb ="https://www.instagram.com/anggatzi123/" //bebas asal jan hapus
+global.youtube = "https://www.instagram.com/anggatzi123/" //bebas asal jan hapus
+global.github = "https://www.instagram.com/anggatzi123/" //bebas
+global.email = "lisabotz150@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['77075223531'] //premium user
+global.owner = ["77075223531","77075223531"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "77075223531" // nomor wa kalian
+global.ownernomerr = "77075223531" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© Adelbot-MD~' //sticker wm ubah
+global.author = 'Di Buat Oleh 𝓛𝓸𝓵𝓲𝓒𝓱𝓪𝓷රධ' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
 global.limitawal = {
     premium: "Infinity",
-    free: 100
+    free: 30
 }
 
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -57,7 +57,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
+    sukses: '🤗Done, Oke Desu jangan lupa follow ig ownerku https://www.instagram.com/anggatzi123/',
     admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
     botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
     owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
